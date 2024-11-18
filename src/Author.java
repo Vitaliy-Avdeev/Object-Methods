59521a0198fda15658e5a90c3ec945afd9b0f842
Vitaliy-Avdeev <avd.vit78@gmail.com>
2024-11-18 09:07:45 +0300

    Mission completed

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,23 @@
 public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
 }
+
